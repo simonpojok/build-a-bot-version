90dde30c04f066ae96cbdee7e4c45a92a400c341
simonojok19 <simonojok19@gmail.com>
2020-09-01 09:43:00 +0300

    typed some styles on the app component

--- src/App.vue
+++ src/App.vue
@@ -40,4 +40,27 @@ main {
   width: 1024px;
   min-height: 300px;
 }
+
+header {
+  background-color: #999;
+  width: 1084px;
+  min-height: 300px;
+}
+
+ul {
+  padding: 3px;
+  display: flex;
+}
+
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+
+.logo {
+  vertical-align: middle;
+  height: 30px;
+}
 </style>
