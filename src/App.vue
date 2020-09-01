5260b5ef09c02344b1bcf6ee5d06912fda489f8f
simonojok19 <simonojok19@gmail.com>
2020-09-01 08:58:46 +0300

    done editing app component

--- src/App.vue
+++ src/App.vue
@@ -18,12 +18,10 @@ export default {
 </script>
 
 <style>
+body {
+  background: linear-gradient(to bottom, #555, #999) fixed;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
