fabb98179c6f4cb956e67b2e57e47ab07ef7c2db
simonojok19 <simonojok19@gmail.com>
2020-09-01 09:00:31 +0300

    added the stylings

--- src/App.vue
+++ src/App.vue
@@ -24,4 +24,11 @@ body {
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
 }
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
+}
 </style>
