a5668bd33b288a06dafc2c88714308ae28f34126
simonojok19 <simonojok19@gmail.com>
2020-09-03 11:47:29 +0300

    v-if and v-show

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,7 +2,10 @@
   <div>
     <div class="top-row">
       <div class="top part">
-        <div class="robot-name">{{ selectedRobot.head.title }}</div>
+        <div class="robot-name">
+          {{ selectedRobot.head.title }}
+          <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
+        </div>
         <img  v-bind:src="selectedRobot.head.src" title="head"/>
         <button v-on:click="selectPreviousHead()" class="prev-selector">&#9668;</button>
         <button v-on:click="selectNextHead()" class="next-selector">&#9658;</button>
@@ -234,5 +237,8 @@ export default {
   top: -25px;
   text-align: center;
   width: 100%;
+}
+.sale {
+  color: red;
 }
 </style>
