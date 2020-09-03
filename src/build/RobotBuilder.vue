3375ad47b0c65b095de50c6a6c79f34b079013d8
simonojok19 <simonojok19@gmail.com>
2020-09-03 11:44:31 +0300

    added interpolations

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,6 +2,7 @@
   <div>
     <div class="top-row">
       <div class="top part">
+        <div class="robot-name">{{ selectedRobot.head.title }}</div>
         <img  v-bind:src="selectedRobot.head.src" title="head"/>
         <button v-on:click="selectPreviousHead()" class="prev-selector">&#9668;</button>
         <button v-on:click="selectNextHead()" class="next-selector">&#9658;</button>
@@ -227,5 +228,11 @@ export default {
 }
 .right .next-selector {
   right: -3px;
+}
+.robot-name {
+  position: absolute;
+  top: -25px;
+  text-align: center;
+  width: 100%;
 }
 </style>
