3f39b5af96c17b0c2dad932e920cc304577d74b4
simonojok19 <simonojok19@gmail.com>
2020-09-03 22:18:19 +0300

    added the scss sass-loader node-sass

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -4,7 +4,7 @@
       Add to Cart
     </button>
     <div class="top-row">
-      <div class="top part" :style="headBorderStyle">
+      <div class="top part" :class="{ 'sale-border': selectedRobot.head.onSale }">
         <div class="robot-name">
           {{ selectedRobot.head.title }}
           <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
