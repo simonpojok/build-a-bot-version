2d27b055c7216c83db305a7ea986b539a43cc1cc
simonojok19 <simonojok19@gmail.com>
2020-09-03 23:30:54 +0300

    added preview html

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="content">
+    <div class="preview">
+      <div class="preview-content">
+        <div class="top-row">
+          <img :src="selectedRobot.head.src"/>
+        </div>
+        <div class="middle-row">
+          <img :src="selectedRobot.leftArm.src" class="rotate-left"/>
+          <img :src="selectedRobot.torso.src"/>
+          <img :src="selectedRobot.rightArm.src" class="rotate-right"/>
+        </div>
+        <div class="bottom-row">
+          <img :src="selectedRobot.base.src"/>
+        </div>
+      </div>
+    </div>
     <button class="add-to-cart" @click="addToCart()">
       Add to Cart
     </button>
