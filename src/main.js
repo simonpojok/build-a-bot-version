e23e352d64630dd8567ddd48a9742c8d4149fdf2
simonojok19 <simonojok19@gmail.com>
2020-09-05 15:47:32 +0300

    changed the project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import { createApp } from 'vue';
+import Vue from 'vue';
 import App from './App.vue';
-import router from './router/index';
+import router from './router';
 
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
+Vue.config.productionTip = false;
+
+new Vue({
+  render: (h) => h(App),
+  router,
+}).$mount('#app');
