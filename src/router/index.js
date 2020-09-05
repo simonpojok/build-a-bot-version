cf648f026490593ec36f1a7633b12e38ff211a7d
simonojok19 <simonojok19@gmail.com>
2020-09-05 15:14:09 +0300

    updated the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../home/HomePage.vue';
-import RobotBuilder from "@/build/RobotBuilder";
+import RobotBuilder from '../build/RobotBuilder.vue';
 
-Vue.use(Router);
-export default new Router({
+const routerHistory = createWebHistory();
+export default createRouter({
+  history: routerHistory,
   routes: [
     { path: '/', name: 'Home', component: HomePage },
     { path: '/build', name: 'Build', component: RobotBuilder },
