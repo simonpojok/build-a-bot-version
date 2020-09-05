8457124f175e265876ff3814ad6ef543128f03ce
simonojok19 <simonojok19@gmail.com>
2020-09-05 14:18:56 +0300

    added robot builder

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HomePage from "@/home/HomePage";
+import HomePage from '../home/HomePage.vue';
+import RobotBuilder from "@/build/RobotBuilder";
 
 Vue.use(Router);
 export default new Router({
   routes: [
     { path: '/', name: 'Home', component: HomePage },
+    { path: '/build', name: 'Build', component: RobotBuilder },
   ],
 });
