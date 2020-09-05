950af551852af65cf3ad69e0ecb9131a36644896
simonojok19 <simonojok19@gmail.com>
2020-09-05 16:08:25 +0300

    added router-link and class

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,10 @@
       <nav>
         <ul>
           <li class="nav-item">
-            <img class="logo" src="./assets/build-a-bot-logo.png" alt="">
+            <router-link class="nav-link" :to="{name: 'Home'}">
+              <img class="logo" src="./assets/build-a-bot-logo.png" alt="">
+              Build-a-Bot
+            </router-link>
           </li>
         </ul>
       </nav>
@@ -55,5 +58,9 @@ ul {
 .logo {
   vertical-align: middle;
   height: 30px;
+}
+.nav-link {
+  text-decoration: none;
+  color: inherit;
 }
 </style>
