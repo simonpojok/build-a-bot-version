2158e04dff9f5514a33bb2f23d471d25b083a543
simonojok19 <simonojok19@gmail.com>
2020-09-06 21:04:37 +0300

    adding sub routs

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,25 @@ import PartInfo from '@/parts/PartInfo.vue';
 import BrowsePart from '@/parts/BrowsePart.vue';
 import HomePage from '../home/HomePage.vue';
 import RobotBuilder from '../build/RobotBuilder.vue';
+import RobotHeads from '@/parts/RobotHeads';
 
 Vue.use(Router);
 export default new Router({
   routes: [
     { path: '/', name: 'Home', component: HomePage },
     { path: '/build', name: 'Build', component: RobotBuilder },
-    { path: '/parts/browse', name: 'BrowseParts', component: BrowsePart },
+    {
+      path: '/parts/browse',
+      name: 'BrowseParts',
+      component: BrowsePart,
+      children: [
+        {
+          name: 'BrowseHead',
+          path: 'heads',
+          component: RobotHeads,
+        }
+      ],
+    },
     {
       path: '/parts/:partTySpe/:id', name: 'Part', component: PartInfo, props: true,
     },
