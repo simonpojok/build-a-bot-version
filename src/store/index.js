6517f0626896e7c1f4803fd7c415018b7da0d2c1
simonojok19 <simonojok19@gmail.com>
2020-09-07 18:00:33 +0300

    added modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import axios from 'axios';
+import robotModules from './modules/robots';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-
+  modules: {
+    robots: robotModules,
+  },
 });
