1928b7deadb546fce6fb45e41bd619d5a01f8cd0
simonojok19 <simonojok19@gmail.com>
2020-09-07 17:58:54 +0300

    removed some duplicates

--- src/store/index.js
+++ src/store/index.js
@@ -5,33 +5,5 @@ import axios from 'axios';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    cart: [],
-    parts: null,
-  },
-  mutations: {
-    addRobotToCart(state, robot) {
-      state.cart.push(robot);
-    },
-    updateParts(state, parts) {
-      state.parts = parts;
-    },
-  },
-  getters: {
-    cartSaleItems(state) {
-      return state.cart.filter((item) => item.head.onSale);
-    },
-  },
-  actions: {
-    getParts({ commit }) {
-      axios.get('/api/parts')
-        .then((results) => commit('updateParts', results.data))
-        .catch(console.error);
-    },
-    addRobotToCart({ commit, state }, robot) {
-      const cart = [...state.cart, robot];
-      return axios.post('/api/cart', cart)
-        .then(() => commit('addRobotToCart', robot));
-    },
-  },
+
 });
