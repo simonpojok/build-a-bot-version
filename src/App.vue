551ed0c7fa6468ac2e67c3394f23f1910011a9a1
simonojok19 <simonojok19@gmail.com>
2020-09-07 11:17:06 +0300

    added the syles

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,9 @@
             <router-link class="nav-link" to="/cart" exact>
               Cart
             </router-link>
+            <div class="cart-item">
+              {{ cart.length }}
+            </div>
           </li>
         </ul>
       </nav>
@@ -37,6 +40,11 @@
 
 export default {
   name: 'App',
+  computed: {
+    cart() {
+      return this.$store.state.cart;
+    },
+  },
 };
 </script>
 
@@ -95,5 +103,16 @@ ul {
   background-color: gray;
   width: 100px;
   min-height: 300px;
+}
+.cart-item {
+  position: absolute;
+  toggle-group: -5px;
+  right: -9px;
+  font-size: 18px;
+  width: 20px;
+  text-align: center;
+  display: inline-block;
+  border-radius: 100px;
+  background-color: mediumseagreen;
 }
 </style>
