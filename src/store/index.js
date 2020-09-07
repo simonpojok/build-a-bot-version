b9d3b31fa351d81b1f7a81b600c6cc1f30bc223f
simonojok19 <simonojok19@gmail.com>
2020-09-07 18:09:40 +0300

    removed the modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,37 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import robotModules from './modules/robots';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: {
-    robots: robotModules,
+  state: {
+    cart: [],
+    parts: null,
+  },
+  mutations: {
+    addRobotToCart(state, robot) {
+      state.cart.push(robot);
+    },
+    updateParts(state, parts) {
+      state.parts = parts;
+    },
+  },
+  getters: {
+    cartSaleItems(state) {
+      return state.cart.filter((item) => item.head.onSale);
+    },
+  },
+  actions: {
+    getParts({ commit }) {
+      axios.get('/api/parts')
+        .then((results) => commit('updateParts', results.data))
+        .catch(console.error);
+    },
+    addRobotToCart({ commit, state }, robot) {
+      const cart = [...state.cart, robot];
+      return axios.post('/api/cart', cart)
+        .then(() => commit('addRobotToCart', robot));
+    },
   },
 });
