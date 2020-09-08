93b92d2c7eac3b60d7b6f98feae118000dadda18
simonojok19 <simonojok19@gmail.com>
2020-09-08 21:25:49 +0300

    added the console.logs

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,9 +1,10 @@
 export default {
   bind: (element, binding) => {
-    if (binding.arg !== 'position') return;
-    Object.keys(binding.modifiers).forEach((key) => {
+    console.log(binding);
+    Object.keys(binding.values).forEach((key) => {
       // eslint-disable-next-line no-param-reassign
-      element.style[key] = '5px';
+      console.log(key);
+      // element.style[key] = '5px';
     });
     // eslint-disable-next-line no-param-reassign
     element.style.position = 'absolute';
