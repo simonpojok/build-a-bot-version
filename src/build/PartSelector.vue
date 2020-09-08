b8e0953cf3c046ca5fd4e7009f29b3afe5bb5b1c
simonojok19 <simonojok19@gmail.com>
2020-09-08 20:38:36 +0300

    added pin directive

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -3,7 +3,7 @@
     <img @click="showPartInfo()" :src="selectedPart.src" title="arm"/>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span v-pin class="sale" v-show="selectedPart.onSale">Sale!</span>
   </div>
 </template>
 
