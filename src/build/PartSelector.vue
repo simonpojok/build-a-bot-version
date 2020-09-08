8989dc87165d43b456fbd0e65a73832210881097
simonojok19 <simonojok19@gmail.com>
2020-09-08 21:47:36 +0300

    added apply styles

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -3,7 +3,7 @@
     <img @click="showPartInfo()" :src="selectedPart.src" title="arm"/>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span v-pin="{ bottom: '10px', right: '5px' }"
+    <span v-pin="{ bottom: pinPadding, right: pinPadding }"
           class="sale" v-show="selectedPart.onSale">Sale!</span>
   </div>
 </template>
@@ -39,7 +39,7 @@ export default {
     },
   },
   data() {
-    return { selectedPartIndex: 0 };
+    return { selectedPartIndex: 0, pinPadding: '10px' };
   },
   updated() {
     this.emitSelectedPart();
--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,10 +1,17 @@
+function applyStyle(element, binding) {
+  Object.keys(binding.value).forEach((position) => {
+    // eslint-disable-next-line no-param-reassign
+    element.style[position] = binding.value[position];
+  });
+  // eslint-disable-next-line no-param-reassign
+  element.style.position = 'absolute';
+}
+
 export default {
   bind: (element, binding) => {
-    Object.keys(binding.value).forEach((position) => {
-      // eslint-disable-next-line no-param-reassign
-      element.style[position] = binding.value[position];
-    });
-    // eslint-disable-next-line no-param-reassign
-    element.style.position = 'absolute';
+    applyStyle(element, binding);
+  },
+  update: (element, binding) => {
+    applyStyle(element, binding);
   },
 };
