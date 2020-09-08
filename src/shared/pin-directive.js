ea58dcc826232e3ea2912417588bb3e6bd500005
simonojok19 <simonojok19@gmail.com>
2020-09-08 20:45:20 +0300

    removed param reassigement

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,7 +1,10 @@
 export default {
   bind: (element) => {
+    // eslint-disable-next-line no-param-reassign
     element.style.position = 'absolute';
+    // eslint-disable-next-line no-param-reassign
     element.style.bottom = '5px';
+    // eslint-disable-next-line no-param-reassign
     element.style.right = '5px';
   },
 };
