4a54c01d891cf21ea895e8b17f2b9e4cde214aa6
simonojok19 <simonojok19@gmail.com>
2021-12-25 14:16:27 +0300

    implemented v-for and v-if

--- src/components/build/RobotBuilder.vue
+++ src/components/build/RobotBuilder.vue
@@ -2,6 +2,10 @@
   <div>
     <div class="top-row">
       <div class="top part">
+        <div class="robot-name">
+          {{ selectedRobot.head.title }}
+          <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
+        </div>
         <img :src="selectedRobot.head.src" title="head"/>
         <button @click="selectNextHead()" class="prev-selector">&#9668;</button>
         <button @click="selectPreviousHead()" class="next-selector">&#9658;</button>
@@ -223,6 +227,17 @@ export default {
 }
 .right .next-selector {
   right: -3px;
+}
+
+.robot-name {
+  position: absolute;
+  top: -25px;
+  text-align: center;
+  width: 100%;
+}
+
+.sale {
+  color: red;
 }
 
 </style>
