502c0a1efd6b86e0acaad67d6ccb68c0fe119886
simonojok19 <simonojok19@gmail.com>
2021-12-25 14:45:06 +0300

    using v-for for displaying contents

--- src/components/build/RobotBuilder.vue
+++ src/components/build/RobotBuilder.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="content">
+    <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
     <div class="top-row">
       <div class="top part">
         <div class="robot-name">
@@ -35,6 +36,23 @@
         <button @click="selectPreviousBase()" class="next-selector">&#9658;</button>
       </div>
     </div>
+    <div>
+      <h1>Cart</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Robot</th>
+            <th class="cost">Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(robot, index) in cart" :key="index">
+            <td>{{robot.head.title}}</td>
+            <td class="cost">{{robot.cost}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -61,6 +79,7 @@ export default {
       selectedRightArmIndex: 0,
       selectedTorsoIndex: 0,
       selectedBaseIndex: 0,
+      cart: [],
     };
   },
   computed: {
@@ -135,6 +154,12 @@ export default {
         this.availableParts.bases.length,
       );
     },
+    addToCart() {
+      const cost = Object.values(this.selectedRobot)
+        .map((item) => item.cost)
+        .reduce((prev, curr) => prev + curr, 0);
+      this.cart.push({ ...this.selectedRobot, cost });
+    },
   },
 };
 </script>
@@ -142,69 +167,84 @@ export default {
 <style scoped>
 .part {
   position: relative;
-  width:165px;
-  height:165px;
+  width: 165px;
+  height: 165px;
   border: 3px solid #aaa;
 }
+
 .part img {
-  width:165px;
+  width: 165px;
 }
+
 .top-row {
-  display:flex;
+  display: flex;
   justify-content: space-around;
 }
+
 .middle-row {
-  display:flex;
+  display: flex;
   justify-content: center;
 }
+
 .bottom-row {
-  display:flex;
+  display: flex;
   justify-content: space-around;
   border-top: none;
 }
+
 .head {
   border-bottom: none;
 }
+
 .left {
   border-right: none;
 }
+
 .right {
   border-left: none;
 }
+
 .left img {
   transform: rotate(-90deg);
 }
+
 .right img {
   transform: rotate(90deg);
 }
+
 .bottom {
   border-top: none;
 }
+
 .prev-selector {
   position: absolute;
-  z-index:1;
+  z-index: 1;
   top: -3px;
   left: -28px;
   width: 25px;
   height: 171px;
 }
+
 .next-selector {
   position: absolute;
-  z-index:1;
+  z-index: 1;
   top: -3px;
   right: -28px;
   width: 25px;
   height: 171px;
 }
+
 .center .prev-selector, .center .next-selector {
-  opacity:0.8;
+  opacity: 0.8;
 }
+
 .left .prev-selector {
   top: -28px;
   left: -3px;
   width: 144px;
   height: 25px;
 }
+
 .left .next-selector {
   top: auto;
   bottom: -28px;
@@ -212,12 +252,14 @@ export default {
   width: 144px;
   height: 25px;
 }
+
 .right .prev-selector {
   top: -28px;
   left: 24px;
   width: 144px;
   height: 25px;
 }
+
 .right .next-selector {
   top: auto;
   bottom: -28px;
@@ -225,6 +267,7 @@ export default {
   width: 144px;
   height: 25px;
 }
+
 .right .next-selector {
   right: -3px;
 }
@@ -238,6 +281,27 @@ export default {
 
 .sale {
   color: red;
+}
+
+.content {
+  position: relative;
+}
+
+.add-to-cart {
+  position: absolute;
+  right: 30px;
+  width: 220px;
+  padding: 3px;
+  font-size: 16px;
+}
+
+td, th {
+  text-align: left;
+  padding: 5px 20px 5px 5px;
+}
+
+.cost {
+  text-align: right;
 }
 
 </style>
